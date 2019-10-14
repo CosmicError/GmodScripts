58c40901e153e34787c00051107008f4237efc64
CosmicError <47365403+CosmicError@users.noreply.github.com>
2019-10-14 09:19:27 -0400

    Create PacRestrictor.lua

---
+++ PacRestrictor.lua
@@ -0,0 +1,15 @@
+-- Change the ranks in "pacranks" to the ranks you want to allow to have acces to the pac editor
+local pacranks = {
+	["moderators"] = true,
+	["developers"] = true,
+	["donators"] = true,
+	["admins"] = true,
+	["superadmin"] = true,
+	["owners"] = true,
+}
+
+hook.Add("PrePACConfigApply", "PACRankRestrict", function(ply)
+	if not pacranks[ply:GetUserGroup()] then
+        return false
+    end
+end)
