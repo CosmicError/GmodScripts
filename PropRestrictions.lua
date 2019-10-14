2a50f330a70e66c3836bf0c4f3f90ef93d7fa7a5
CosmicError <47365403+CosmicError@users.noreply.github.com>
2019-10-14 09:08:08 -0400

    Create PropRestrictions.lua

---
+++ PropRestrictions.lua
@@ -0,0 +1,30 @@
+-- DO NOT CHANGE HOOKS UNLESS YOU KNOW WHAT YOU ARE DOING
+local hooks = {
+    "Effect",
+    "NPC",
+    "Prop",
+    "Ragdoll",
+    "SENT",
+    "Vehicle"
+}
+
+-- Change Pranks to include all the ranks that can spawn props
+local pranks = {
+    ["builder"] = true,
+    ["moderator"] = true,
+    ["gamemaster"] = true,
+    ["admin"] = true,
+    ["superadmin"] = true,
+    ["developer"] = true,
+    ["manager"] = true,
+}
+
+-- DO NOT CHANGE THIS UNLESS YOU KNOW WHAT YOU ARE DOING!
+for _, v in pairs (hooks) do
+    hook.Add("PlayerSpawn"..v, "Disallow_user_"..v, function(ply)
+        if not pranks[ply:GetUserGroup()] then
+            return false
+        end
+        return true
+    end)
+end
