17b540ac003362a6093b846bb7f770dd07d9ea4c
CosmicError <47365403+CosmicError@users.noreply.github.com>
2019-11-15 07:08:00 -0500

    Update PacRestrictor.lua

--- PacRestrictor.lua
+++ PacRestrictor.lua
@@ -1,6 +1,7 @@
--- make a file in garrysmod/lua/autorun/server
+-- Make a file in garrysmod/lua/autorun/server
 
--- Change the ranks in "pacranks" to the ranks you want to allow to have acces to the pac editor
+-- // USER GROUP VERSION //
+-- Change the groups in "pacranks" to the user groups you want to allow to have acces to the pac editor
 local pacranks = {
 	["moderators"] = true,
 	["developers"] = true,
@@ -12,6 +13,17 @@ local pacranks = {
 
 hook.Add("PrePACConfigApply", "PACRankRestrict", function(ply)
 	if not pacranks[ply:GetUserGroup()] then
+        return false
+    end
+end)
+
+-- // STEAMID3 VERSION //
+local allowid = {
+	["[U:1:355048465]"] = true -- my steamid3
+}
+
+hook.Add("PrePACConfigApply", "PACRankRestrict", function(ply)
+	if not allowid[ply:AccountID()] then
         return false
     end
 end)
